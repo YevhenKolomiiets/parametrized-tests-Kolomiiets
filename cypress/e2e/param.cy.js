369d2a8e10e32d11d51778d7c5f1a6be625a6637
YevhenKolomiiets <eugene.kolomiiets@gmail.com>
2023-07-04 10:28:23 +0300

    Parametrized test

--- cypress/e2e/param.cy.js
+++ cypress/e2e/param.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe ('Parametrized test', ()=> {
+
+  const tests = [{email: '1@gm.io', password: 1111}, {email: '2@gm.io', password: 2222}];
+
+  tests.forEach(({email, password}) => {
+      it(`Test for text`, () => {
+          cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
+          cy.get('[alt="Material Dark Theme"]').click();
+          cy.get('[title="Forms"]').click();
+          cy.get('[title="Form Layouts"]').click();
+          cy.get('#exampleInputEmail1').type(email);
+          cy.get('#exampleInputPassword1').type(password);
+          cy.get('.custom-checkbox ~ span').contains('Check me out').click();
+          cy.get('.status-danger').click
+      })
   })
+  
 })
